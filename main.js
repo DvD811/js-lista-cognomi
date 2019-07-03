cd83ace4c25eaf704447e1ece9b3b7f078715909
DvD811 <davide.gallizioli@gmail.com>
2019-07-03 15:18:03 +0200

    base settiing

---
+++ main.js
@@ -0,0 +1,6 @@
+// * DESCRIZIONE=*
+//     Chiedi all’utente il cognome, inseriscilo in un array con altri cognomi e stampa la lista ordinata alfabeticamente.
+// Scrivi a che posizione della lista il nuovo utente si trova
+
+//     * NOTE=* fate l’output in una ul, come abbiamo visto stamattina(ovviamente nell’ul van sempre dei<li>)
+// non create la lista già in ordine alfabetico
